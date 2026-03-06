a4c99ee053c857e83bfcdce28715090ab86a1373
Kris Gray <kag56@cam.ac.uk>
2026-03-06 13:36:08 +0000

    feat(03-04): register /openpaul:status command
    - Export paulStatus from commands/index.ts
    - Import and register paulStatus in src/index.ts
    - Command registered as 'paul:status' tool

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,11 +4,13 @@
  * Exports all PAUL commands
  */
 
- // Core loop commands
+  // Core loop commands
 export { paulInit } from './init'
 export { paulPlan } from './plan'
 export { paulProgress } from './progress'
+export { paulStatus } from './status'
 export { paulHelp } from './help'
 export { paulApply } from './apply'
 export { paulUnify } from './unify'
 export { paulPause } from './pause'
+export { paulResume } from './resume'
