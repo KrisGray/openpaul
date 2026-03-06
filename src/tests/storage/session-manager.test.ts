75f6641bdcea6aba765b1a21270bdadcac017397
Kris Gray <kag56@cam.ac.uk>
2026-03-06 12:10:41 +0000

    test(03-00a): create SessionManager test scaffold
    - Add describe block for SessionManager class
    - Add placeholder test to satisfy Jest requirement
    - Include TODO comment for implementation in plan 03-06a
    - Establish test file structure before implementation

---
+++ src/tests/storage/session-manager.test.ts
@@ -0,0 +1,16 @@
+/**
+ * SessionManager Tests
+ * 
+ * Tests for SessionManager class
+ * 
+ * TODO: Tests to be implemented in plan 03-06a
+ */
+
+describe('SessionManager class', () => {
+  // TODO: Tests to be implemented in plan 03-06a
+  
+  it('placeholder test - tests will be implemented in plan 03-06a', () => {
+    // This is a scaffold test to satisfy Jest requirement for at least one test
+    expect(true).toBe(true)
+  })
+})
