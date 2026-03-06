64c1c0d556a27bc32c250ecb1b432d50c1cf8926
Kris Gray <kag56@cam.ac.uk>
2026-03-06 12:14:17 +0000

    test(03-00b): add test scaffold for diff-formatter utility
    - Added describe block for diff-formatter utility
    - Added todo tests for future implementation
    - Satisfies Nyquist compliance (tests before implementation)
    - Tests will be implemented in plan 03-06b

---
+++ src/tests/output/diff-formatter.test.ts
@@ -0,0 +1,14 @@
+/**
+ * Diff Formatter Tests (Scaffold)
+ * 
+ * Tests for diff formatting functionality
+ * TODO: Tests to be implemented in plan 03-06b
+ */
+
+describe('diff-formatter utility', () => {
+  it.todo('should format unified diffs for display')
+  it.todo('should handle file additions')
+  it.todo('should handle file deletions')
+  it.todo('should handle file modifications')
+  it.todo('should format diff stats')
+})
